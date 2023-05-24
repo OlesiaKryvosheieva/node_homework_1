b8c8df283b1ec0034ca5c494a56793bf99ad1cf6
OlesiaKryvosheieva <110483217+OlesiaKryvosheieva@users.noreply.github.com>
2023-05-24 02:30:29 +0200

    init project, wrote functions and export them

---
+++ contacts.js
@@ -0,0 +1,49 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const listContacts = async () => {
+  const listContacts = await fs.readFile(contactsPath);
+  return JSON.parse(listContacts);
+};
+
+const getContactById = async (contactId) => {
+  const allContacts = await listContacts();
+  const contactById = allContacts.findIndex(
+    (contact) => (contact.id = contactId)
+  );
+  return contactById || null;
+};
+
+const removeContact = async (contactId) => {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((contact) => (contact.id = contactId));
+  if (index === -1) {
+    return null;
+  }
+  const [result] = allContacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return result;
+};
+
+const addContact = async (name, email, phone) => {
+  const allContacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  allContacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
